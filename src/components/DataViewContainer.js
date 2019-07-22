4b8b568ed704b9ea52e1a994f5f5906a3b09d8ec
rjgao1 <rooney.j.gao@gmail.com>
2019-07-22 00:53:37 -0400

    now tooltip toggle switch is working

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import _ from 'lodash'
 import { ShotChart } from './ShotChart'
 import { CountSlider } from './CountSlider'
-import { Radio, Row, Col } from 'antd'
+import { Radio, Row, Col, Switch } from 'antd'
 
 import "../styles/DataViewContainer.css"
 
@@ -14,6 +14,7 @@ export class DataViewContainer extends React.Component {
   state = {
     minCount: 2,
     chartType: 'hexbin',
+    displayToolTips: true,
   }
 
   onCountSliderChange = (count) => {
@@ -28,25 +29,49 @@ export class DataViewContainer extends React.Component {
     this.setState({chartType: e.target.value})
   }
 
+  onToolTipChange = (displayToolTips) => {
+    console.log(displayToolTips)
+    this.setState({ displayToolTips })
+  }
+  // onToolTipChange = (displayToolTips) => {
+  //   this.setState({ displayToolTips })
+  //   console.log(this.state.displayToolTips)
+  // }
+
   render() {
     return (
       <div className="data-view">
         <ShotChart 
           playerId={this.props.playerId}
-          // minCount={this.state.minCount}
+          minCount={this.state.minCount}
           chartType={this.state.chartType}
+          displayToolTips={this.state.displayToolTips}
         />
         <div className="filters">
-          <CountSlider 
-            value={this.state.minCount}
-            onCountSliderChange={this.onCountSliderChange}
-          />
+          {
+            this.state.chartType === 'hexbin' ?
+            <CountSlider 
+              value={this.state.minCount}
+              onCountSliderChange={_.debounce(this.onCountSliderChange, 500)}
+            /> 
+            : null
+          }
+          <br/>
+          
           <Row>
             <Col span={9}>
               <RadioGroup onChange={this.onChartTypeChange} value={this.state.chartType}>
                 <Radio value="hexbin">Hexbin</Radio>
                 <Radio value="scatter">Scatter</Radio>
               </RadioGroup>
+            </Col>
+            <Col span={4}>
+              <Switch
+                checkedChildren="On"
+                unCheckedChildren="Off"
+                onChange={this.onToolTipChange}
+                defaultChecked
+              />
             </Col>
           </Row>
         </div>
--- src/components/ShotChart.js
+++ src/components/ShotChart.js
@@ -12,10 +12,13 @@ window.d3_hexbin = {hexbin: hexbin} // workaround library problem
 export class ShotChart extends React.Component {
   static propTypes = {
     playerId: PropTypes.number,
+    minCount: PropTypes.number,
     chartType: PropTypes.string,
+    displayToolTips: PropTypes.bool,
   }
 
   componentDidUpdate() {
+    console.log("ShotChart ToolTips: ", this.props.displayToolTips)
     nba.stats.shots({
       PlayerID: this.props.playerId
     }).then((response) => {
@@ -28,10 +31,12 @@ export class ShotChart extends React.Component {
       }))
 
       const courtSelection = d3.select("#shot-chart")
+      // without this line, all updates on court would be ineffect only after changing chartType
+      courtSelection.html('')
       const chart_court = court().width(500)
       const chart_shots = shots()
-                            .shotRenderThreshold(2)
-                            .displayToolTips(true)
+                            .shotRenderThreshold(this.props.minCount)
+                            .displayToolTips(this.props.displayToolTips)
                             .displayType(this.props.chartType)
       // selection.call always return the selection and not the return value of function passed in
       courtSelection.call(chart_court)
