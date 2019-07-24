0b938394b874b4b679ca83a4eeaf80aa070aef08
rjgao1 <rooney.j.gao@gmail.com>
2019-07-24 17:04:29 -0400

    added search bar and finished search function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Profile } from './Profile'
-import { ShotChart } from './ShotChart'
 import nba from 'nba'
 import { DEFAULT_PLAYER_INFO } from '../constants'
 
@@ -31,7 +30,7 @@ export class Main extends React.Component {
   render() {
     return (
       <div className="main">
-        <SearchBar />
+        <SearchBar loadPlayerInfo={this.loadPlayerInfo}/>
         <div className="player">
           <Profile playerInfo={this.state.playerInfo} />
           <DataViewContainer playerId={this.state.playerInfo.playerId}/>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,9 +9,40 @@ export class SearchBar extends React.Component {
     dataSource: [],
   }
 
+  handleSearch = (value) => {
+    console.log(value)
+    this.setState({
+      dataSource: !value ? [] : nba.searchPlayers(value).map(
+        ({ fullName, playerId }) => 
+          <Option key={playerId} value={fullName}>
+            <img 
+              className="player-option-image" 
+              src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
+              alt="player option profile"
+            />
+            <span className="player-optiont-label">{fullName}</span>
+          </Option>
+      )
+    })
+  }
+
+  onSelect = (playerName) => {
+    this.props.loadPlayerInfo(playerName)
+  }
+
   render() {
+
+    const { dataSource } = this.state
     return (
-      <AutoComplete className="search-bar">
+      <AutoComplete 
+        className="search-bar"
+        size="large"
+        placeholder="Search NBA players here"
+        optionLabelProp="value"
+        dataSource={dataSource}
+        onSearch={this.handleSearch}
+        onSelect={this.onSelect}
+      >
 
         <Input suffix={<Icon type="search" className="certain-category-icon"/>}/>
         
